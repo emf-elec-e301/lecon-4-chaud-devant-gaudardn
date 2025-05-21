0468e5e2e3e438c1401d08558649d50c0f4b6c56
gaudardn <nicolas.gaudard@studentfr.ch>
2025-05-21 09:13:27 +0200

    Chaud devant

--- main.c
+++ main.c
@@ -25,13 +25,21 @@ int main(void)
     // VOTRE CODE CI-DESSOUS
     // INSTRUCTIONS EXECUTEES UNE SEULE FOIS AU DEMARRAGE
     //----------------------------------------------------------
-    
+    int yeux; 
     while(1){
         //----------------------------------------------------------
         // VOTRE CODE CI-DESSOUS
         // INSTRUCTIONS EXECUTEES EN BOUCLE INDEFINIMENT
         //----------------------------------------------------------
-        
+        yeux=ultrason_distance();
+        if (yeux >= 10)//SI C'EST PLUS PETIT QUE 10 CM 
+        {
+            buzzer_sing(SING_HAPPY);//LE BUZZER JOUE LA MUSIQUE HAPPY 
+        }
+        else//sinon
+        {
+            buzzer_sing(SING_SAD);//LE BUZZER JOUE LA MUSIQUE SAD 
+        }
     }
 }
 //----------------------------------------------------------
